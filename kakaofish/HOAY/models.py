bd71b6fe758afd3b99606c206942243bb432f3cf
hooong <tjrwns0529@gmail.com>
2020-01-06 19:03:56 +0900

    create model

--- kakaofish/HOAY/models.py
+++ kakaofish/HOAY/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class face_img(models.Model):
+    idNum = models.IntegerField()
+    faceImg = models.ImageField(upload_to="face_img")
+    
+class predict_age(models.Model):
+    realAge = models.IntegerField()
+    predictAge = models.IntegerField()
