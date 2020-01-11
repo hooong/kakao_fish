64c032c26f2eb608a0379d291777697834415caf
hooong <tjrwns0529@gmail.com>
2020-01-12 02:41:18 +0900

    fix h percent

--- kakaofish/HOAY/views.py
+++ kakaofish/HOAY/views.py
@@ -86,7 +86,7 @@ def predictAge(request, id):
     gender = faceInfo['gender']
     m = float(gender['male'])
     f = float(gender['female'])
-    if abs(m - f) < 10: 
+    if abs(m - f) < 6: 
         gender = 'h' 
     else:
         if m > f:
