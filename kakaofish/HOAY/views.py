f3e45e198b002fb9f457f8ab0a1a0af889f081fe
hooong <tjrwns0529@gmail.com>
2020-01-11 23:50:40 +0900

    add tier System

--- kakaofish/HOAY/views.py
+++ kakaofish/HOAY/views.py
@@ -29,28 +29,27 @@ def result(request, id):
     gender = pre.gender
     realage = pre.realAge
     age = pre.predictAge
+    diff = pre.diff
 
     if gender == 'm':
         gender = '남자'
     else:
         gender = '여자'
-
-    age_distance = int(realage) - int(age)
-    age_dis = abs(age_distance)
     
     # 동안 or 노안
-    if age_distance < 0:
+    if diff > 0:
         young_or_older = 'older'
-    elif age_distance > 0:
+    elif diff < 0:
         young_or_older = 'younger'
     else:
         young_or_older = 'same'
 
     # 순위 시스템
-    
+    tier, percent = tierSystem(diff)
 
-
-    context = {'gender': gender, 'age': age, 'young_or_older': young_or_older, 'age_dis': age_dis}
+    context = {'gender': gender, 'age': age, 
+                'young_or_older': young_or_older, 'age_dis': abs(diff),
+                'tier': tier, 'percent': percent}
     return render(request, "result.html", context)
 
 def error(request):
@@ -95,9 +94,37 @@ def predictAge(request, id):
     tmp_predict = Predict_age()
     tmp_predict.predictAge = age
     tmp_predict.realAge = realAge 
-    tmp_predict.diff = abs(int(realAge)-int(age))
+    tmp_predict.diff = int(age) - int(realAge)
     tmp_predict.gender = gender
     tmp_predict.save()
     pre_id = tmp_predict.id
 
     return redirect('/hoay/result/' + str(pre_id))
+
+# 계급 시스템
+def tierSystem(diff):
+
+    allCount = Predict_age.objects.count()
+    print(allCount)
+    diffCount = Predict_age.objects.filter(diff__lte=diff).count()
+    print(diffCount)
+
+    percent = int((diffCount / allCount) * 100)
+    print(percent)
+
+    if percent < 5:
+        tier = 'Master'
+    elif percent < 15:
+        tier = 'Diamond'
+    elif percent < 30:
+        tier = 'Platinum'
+    elif percent < 50:
+        tier = 'Gold'
+    elif percent < 80:
+        tier = 'Silver'
+    elif percent < 95:
+        tier = 'Bronze'
+    else:
+        tier = 'Iron'
+
+    return tier, percent
