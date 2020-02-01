d36f068d4a8d808a5aadda1744da307d55d8d585
hooong <tjrwns0529@gmail.com>
2020-02-01 17:59:49 +0900

    add news크롤링

--- kakaofish/nCov/views.py
+++ kakaofish/nCov/views.py
@@ -14,6 +14,19 @@ def coIndex(request):
     # p_table = soup.find("table", {"class":"table table-bordered"})
     # print(p_table)
 
-    context = {}
+    # 뉴스 크롤링
+    # 스케줄러로 돌리고 db에 저장하고 db에서 상위 몇개씩 가져오면 된다.
+    url = "https://openapi.naver.com/v1/search/news.json"
+    query = "우한폐렴"
+
+    request_url = url + '?query=' + query + '&display=15'
+    Client_Id = "sIROnlLXckZke3JED_1d"
+    Client_Secret = "LeMrSZcrCx"
+    headers = {'X-Naver-Client-Id': Client_Id, "X-Naver-Client-Secret": Client_Secret}
+
+    response_url = requests.get(request_url, headers=headers)
+    news = response_url.json()
+
+    context = {'news':news['items']}
 
     return render(request, 'index.html', context)
