1c28d0bd7bc46313a1db55905d545b132d91641e
hooong <tjrwns0529@gmail.com>
2020-02-16 13:58:10 +0900

    lotto_V_1.0

--- kakaofish/lotto/views.py
+++ kakaofish/lotto/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+import random
 
 def start_btn(request):
     return render(request, 'start_btn.html')
 
 def get_number(request):
-    return render(request, 'show_number.html')
+    nums = []
+    order = ['A', 'B', 'C', 'D', 'E']
+    
+    for _ in range(5):
+        tmp = []
+        cnt = 0
+        while cnt != 6:
+            tmp_num = random.randrange(1,46)
+            if not tmp_num in tmp:
+                tmp.append(tmp_num)
+                cnt += 1
+        nums.append(tmp)
+    
+    get_num = zip(order, nums)
+
+
+    context = {'nums':get_num}
+    return render(request, 'show_number.html', context)
