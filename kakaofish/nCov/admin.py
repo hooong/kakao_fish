d5fa449149d7969aaa52a085614dd379a8902360
hooong <tjrwns0529@gmail.com>
2020-02-26 21:59:29 +0900

    fix

--- kakaofish/nCov/admin.py
+++ kakaofish/nCov/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(News)
-admin.site.register(Board)
-
-
-
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'registered_date',)
 
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Board)
