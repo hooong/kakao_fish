d67ffa007b51ca7326c6f5b180d6d9aa8ec8adfb
park <pannchat@likelion.org>
2020-02-27 15:12:56 +0900

    크롤링:

--- kakaofish/nCov/scheduler.py
+++ kakaofish/nCov/scheduler.py
@@ -5,12 +5,12 @@ import requests
 # 스케쥴러 생성
 def schedule():
     sched = BackgroundScheduler()
-    sched.add_job(crawling_news, 'cron', minute='0,30')
+    sched.add_job(crawling_news, 'cron', minute='0,30,15')
     sched.start()
 
 def crawling_news():
     url = "https://openapi.naver.com/v1/search/news.json"
-    query = "우한폐렴"
+    query = "코로나 미담"
 
     request_url = url + '?query=' + query + '&display=10'
     Client_Id = "sIROnlLXckZke3JED_1d"
@@ -19,7 +19,13 @@ def crawling_news():
 
     response_url = requests.get(request_url, headers=headers)
     news = response_url.json()
-
+    n=len(news['items'])
+    for i in range(n):
+            for key,value in news['items'][i].items():
+                    news['items'][i][key]=news['items'][i][key].replace("<b>","")
+                    news['items'][i][key]=news['items'][i][key].replace("</b>","")
+                    news['items'][i][key]=news['items'][i][key].replace("&quot;","")
+                    print(news['items'][i][key])
     news = news['items']
     for new in news:
         save_new = News()
--- kakaofish/nCov/views.py
+++ kakaofish/nCov/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import urllib.request
 # from selenium import webdriver
 
 import requests
@@ -8,38 +9,66 @@ from .models import *
 # from tag.models import Tag
 import re
 def coIndex(request):
-    # driver = webdriver.Firefox()
-    # driver.get("https://thewuhanvirus.com/")
-    
-    # html = driver.page_source
-    # soup = BeautifulSoup(html)
+        # driver = webdriver.Firefox()
+        # driver.get("https://thewuhanvirus.com/")
+        
+        # html = driver.page_source
+        # soup = BeautifulSoup(html)
 
-    # p_table = soup.find("table", {"class":"table table-bordered"})
-    # print(p_table)
+        # p_table = soup.find("table", {"class":"table table-bordered"})
+        # print(p_table)
 
-    # 뉴스 크롤링
-    # 스케줄러로 돌리고 db에 저장하고 db에서 상위 몇개씩 가져오면 된다.
-    url = "https://openapi.naver.com/v1/search/news.json"
-    query = "코로나"
+        # 뉴스 크롤링
+        # 스케줄러로 돌리고 db에 저장하고 db에서 상위 몇개씩 가져오면 된다.
+        url = "https://openapi.naver.com/v1/search/news.json"
+        query = "코로나"
 
-    request_url = url + '?query=' + query + '&display=15'
-    Client_Id = "sIROnlLXckZke3JED_1d"
-    Client_Secret = "LeMrSZcrCx"
-    headers = {'X-Naver-Client-Id': Client_Id, "X-Naver-Client-Secret": Client_Secret}
+        request_url = url + '?query=' + query + '&display=15'
+        Client_Id = "sIROnlLXckZke3JED_1d"
+        Client_Secret = "LeMrSZcrCx"
+        headers = {'X-Naver-Client-Id': Client_Id, "X-Naver-Client-Secret": Client_Secret}
 
-    response_url = requests.get(request_url, headers=headers)
-    news = response_url.json()
-    n=len(news['items'])
-    for i in range(len(news['items'])):
-        for key,value in news['items'][i].items():
-            news['items'][i][key]=news['items'][i][key].replace("<b>","")
-            news['items'][i][key]=news['items'][i][key].replace("</b>","")
-            news['items'][i][key]=news['items'][i][key].replace("&quot;","")
-            # print(news['items'][i][key])
-    news = News.objects.all().order_by('-id')[:10]
-    # print(news)
-    context = {'news':news}
-    boards = Board.objects.order_by('-id')
+        response_url = requests.get(request_url, headers=headers)
+        news = response_url.json()
+        n=len(news['items'])
+        for i in range(len(news['items'])):
+                for key,value in news['items'][i].items():
+                        news['items'][i][key]=news['items'][i][key].replace("<b>","")
+                        news['items'][i][key]=news['items'][i][key].replace("</b>","")
+                        news['items'][i][key]=news['items'][i][key].replace("&quot;","")
+                # print(news['items'][i][key])
+        news = News.objects.all().order_by('-id')[:10]
+        # print(news)
+        context = {'news':news}
+        boards = Board.objects.order_by('-id')
 
-    return render(request, 'index.html', {'context':context,'boards':boards})
+        return render(request, 'index.html', {'context':context,'boards':boards})
 
+
+def goodnews(request):
+        url = "https://openapi.naver.com/v1/search/news.json"
+        query = "코로나 미담"
+
+        request_url = url + '?query=' + query + '&display=15'
+        Client_Id = "sIROnlLXckZke3JED_1d"
+        Client_Secret = "LeMrSZcrCx"
+        headers = {'X-Naver-Client-Id': Client_Id, "X-Naver-Client-Secret": Client_Secret}
+
+        response_url = requests.get(request_url, headers=headers)
+        news = response_url.json()
+        n=len(news['items'])
+        for i in range(n):
+                for key,value in news['items'][i].items():
+                        news['items'][i][key]=news['items'][i][key].replace("<b>","")
+                        news['items'][i][key]=news['items'][i][key].replace("</b>","")
+                        news['items'][i][key]=news['items'][i][key].replace("&quot;","")
+                        # print(news['items'][i][key])
+
+        # print(news)
+
+
+
+        news = News.objects.all().order_by('-id')[:10]
+        # print(news)
+        context = {'news':news}
+        return render(request, 'goodnews.html',context)
